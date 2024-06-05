32b7ec477c9085104ff89da82da9c618b530e206
Somdipto Chakraborty <108862434+trap-representation@users.noreply.github.com>
2024-06-05 21:43:32 +0530

    Modified comments in vigilante/src/vconfig.h

--- vigilante/src/vconfig.h
+++ vigilante/src/vconfig.h
@@ -1,9 +1,11 @@
 #ifndef VCONFIG_H
 #define VCONFIG_H
 
-/* Redefine WORD_SIZE to whatever the size of a word on your machine is (in bytes) */
-
+/* Redefine WORD_SIZE (in bytes) depending on your implementation */
 #define WORD_SIZE 2
+
+/* Comment out one of ENDIAN_LITTLE and ENDIAN_BIG, depending on your implementation */
 #define ENDIAN_LITTLE
+/* #define ENDIAN_BIG */
 
 #endif
